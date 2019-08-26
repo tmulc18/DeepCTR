2a8d93f7b0771d4ac447dd85a0d4e1f0ede917e0
tmulc18 <tmulc18@gmail.com>
2019-08-26 22:04:36 +0000

    tested s3 partition download

--- deepctr/dist_utils/download_utils.py
+++ deepctr/dist_utils/download_utils.py
@@ -1,10 +1,18 @@
 import os
+import random
 import boto3
 from tqdm import tqdm
 from multiprocessing import Pool
 from multiprocessing.pool import ThreadPool
 
-def download_partition_s3(worker_id, num_workers, save_dir, bucket, prefix, nthreads):
+def download_partition_s3(worker_id, num_workers, save_dir, bucket, prefix, nthreads, nval=200):
+    """Downloads a partition of data from s3.
+
+    If the buckets has L files, there are V files for validation,
+    and there are W workers, this function downloads (L-V)/W 
+    files for trainng and V files for validation.
+    """
+    
     resource = boto3.resource('s3') #high-level object-oriented API
     my_bucket = resource.Bucket(bucket) #subsitute this for your s3 bucket name. 
 
@@ -17,19 +25,44 @@ def download_partition_s3(worker_id, num_workers, save_dir, bucket, prefix, nthr
 
     # partition
     fkeys.sort() # must sort so multiple workers can take subsets
+
+    # randomize, but set seed so all workers randomize the same
+    random.seed(1)
+    random.shuffle(fkeys)
+
+    # all workers get the same validation set
+    fkeys_val = fkeys[:nval]
+
+    # take out the validation data from the training keys
+    fkeys = fkeys[nval:]
+
+    # partition training data
     fkeys = fkeys[worker_id::num_workers]
 
-    # create location locally
+    # create location train locally
     if not os.path.exists(save_dir): os.mkdir(save_dir) 
+    if not os.path.exists(save_dir+"_val"): os.mkdir(save_dir+"_val") 
 
     # define download function
     def download_func(fkey):
+        print("Downloading training, ",fkey)
         my_bucket.download_file(fkey,save_dir+'/'+fkey.split('/')[-1])
 
-    # download
+    # define download function for validation
+    def download_func_val(fkey):
+        my_bucket.download_file(fkey,save_dir+"_val"+'/'+fkey.split('/')[-1])
+
+    # download training data
     p = ThreadPool(nthreads)
     total_files = len(fkeys)
     with tqdm(total=total_files) as pbar:
         for i,_ in tqdm(enumerate(p.imap_unordered(download_func,fkeys))):
+            pbar.update()
+
+    # download validation data
+    p = ThreadPool(nthreads)
+    total_files = len(fkeys_val)
+    with tqdm(total=total_files) as pbar:
+        for i,_ in tqdm(enumerate(p.imap_unordered(download_func_val,fkeys_val))):
             pbar.update()
 
